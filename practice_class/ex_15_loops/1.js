668685e33aa70fe4b9f055f00daa3ba507c264f4
JannikFlaig <128989434+JannikFlaig@users.noreply.github.com>
2023-05-04 09:46:16 +0200

    Loops

--- practice_class/ex_15_loops/1.js
+++ practice_class/ex_15_loops/1.js
@@ -2,55 +2,105 @@ let res = 0;
 let ges = 0;
 
 function calculate() {
-    let select = document.querySelector("#select").value;
-    let n = document.querySelector("#n").value;
+    let select = document.getElementById("select").value;
+    let n = document.getElementById("n").value;
+
+    if(select == 1) {
+        series1 = document.createTextNode("Fn = n * n; F1 = 1");
+        document.getElementById("outcome").appendChild(series1);
+        br = document.createElement('br');
+        document.getElementById("outcome").appendChild(br);
+    } else if(select == 2) {
+        series2 = document.createTextNode("Fn = Fn-1 * 2; F1 = 1")
+        document.getElementById("outcome").appendChild(series2);
+        br = document.createElement('br');
+        document.getElementById("outcome").appendChild(br);
+    } else if(select == 3){
+        series3 = document.createTextNode("Fn = Fn-1 + Fn-2; F1 = F2 = 1");
+        document.getElementById("outcome").appendChild(series3);
+        br = document.createElement('br');
+        document.getElementById("outcome").appendChild(br);
+    } else if(select == 4) {
+        series4 = document.createTextNode("Fn = nth prime number; F1 = 2");
+        document.getElementById("outcome").appendChild(series4);
+        br = document.createElement('br');
+        document.getElementById("outcome").appendChild(br);
+    }
+
     console.log("select:", select);
     console.log("n:", n);
     if(select == 1) {
-        let m = n; 
-        for(let i = 0; i < n; i++) {
-            // console.log(i);
-            // console.log(m);
-            ges = m * m;
-            console.log(ges);
+        for(let i = 1; i <= n; i++) {
+            ges = i * i;
             text = document.createTextNode(ges);
             br = document.createElement('br');
             document.getElementById("outcome").appendChild(text);
             document.getElementById("outcome").appendChild(br);
-            m = m - 1;
         }
+
     } else if(select == 2) {
         let F1 = 1;
-        for(i = 1; i < n; i++) {
+        for(i = 0; i < n; i++) {
+            text = document.createTextNode(F1);
+            br = document.createElement('br');
+            document.getElementById("outcome").appendChild(text);
+            document.getElementById("outcome").appendChild(br);            
             F1 = F1 * 2;
         }
-        res = F1;
+
     } else if(select == 3) {
         let a = 0;
         let b = 1;
         
-        // For-Loop, um die Fibonacci-Reihe zu berechnen und auszugeben
         for (let i = 0; i < n; i++) {
-          console.log(a);
+          text = document.createTextNode(b);
+          br = document.createElement('br');
+          document.getElementById("outcome").appendChild(text);
+          document.getElementById("outcome").appendChild(br);          
           const next = a + b;
           a = b;
           b = next;
         }
-    } else if(select == 4) {
-        //in working
-    }
 
-    // console.log(res);
+
+
+    } else if(select == 4) {
+        nthPrime();
+        let nth = nthPrime(n);
+        text = document.createTextNode(nth);
+        br = document.createElement('br');
+        document.getElementById("outcome").appendChild(text);
+        document.getElementById("outcome").appendChild(br);
+    }
+    br = document.createElement('br');
+    document.getElementById("outcome").appendChild(br);
 }
 
-
-
-
-
-
-
-
-
-//while schleife die schaut ob die n-te zahl gefunden ist. Darin eine for-schleife die:
-//1. ab zwei anfängt zu zählen
-//2. primzahlen rausfindet und für jede primzahl k + 1 rechnnet(while)
+function prime(num) { 
+    //testing number if prime
+    for(let i = 2; i < num; i++) {
+      if(num % i == 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+  
+  function nthPrime(n) {
+//searcching for nth prime number
+    let count = 0;
+    let num = 2;
+    while(count < n) {
+      if(prime(num) == true) {
+        count++;
+        if(count == n) {
+          return num; //nth prime number
+        }
+        text = document.createTextNode(num);
+        br = document.createElement('br');
+        document.getElementById("outcome").appendChild(text);
+        document.getElementById("outcome").appendChild(br); 
+      }
+      num++;
+    }
+  }
